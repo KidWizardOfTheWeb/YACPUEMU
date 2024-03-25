affa6734f94bf8416478d480f4016c0e7adb0f5b
KidWizardOfTheWeb <smasih2020@gmail.com>
2024-03-25 15:58:29 -0400

    Added notes on CPUView, started controller basis

--- src/controllers/CPUController.py
+++ src/controllers/CPUController.py
@@ -6,5 +6,24 @@ class CPUController:
         pass
 
     def cycle(self):
+        inputFile = open(sys.argv[1], "r") # The binary we must read
 
+        '''
+        The cycle behavior flag essentially will tell the loop to run by stepping on the user's command,
+        or automatically if set to 1 at the beginning
+        '''
+        cycleBehaviorFlag = -1
+        while cycleBehaviorFlag is not 0 or cycleBehaviorFlag is not 1:
+            cycleBehaviorFlag = input("Type 0 to step through the program or type 1 to run it all at once.")
+
+        if cycleBehaviorFlag is 1:
+            userStep = 1
+        else:
+            userStep = 0
+        # If cycle behavior flag is not 0, it will run the whole program at once
+        while (cycleBehaviorFlag != 0):
+            if (cycleBehaviorFlag == 0 and userStep == 1):
+                # Manual stepping here
+                pass
+            pass
         pass
--- src/views/CPUView.py
+++ src/views/CPUView.py
@@ -9,3 +9,14 @@ class CPUView:
     def setController(self, c):
         controller = c
         pass
+
+    '''
+    class CPUView:
+	def __init__(self):
+		self.PC = PC # PC counter. Starts at 0x0.
+		self.registerArr = [31] # all 31 registers, r0-r31
+		self.memArr = memArr # Emulate memory, somehow.
+		self.ALUCount = ALUCount # Count arithimetic operations (note: there are some beyond the obvious, ex. beq)
+		self.readWriteCount = readWriteCount # num of reads (load byte/half/word) and writes
+		self.instrCount = instrCount
+    '''
