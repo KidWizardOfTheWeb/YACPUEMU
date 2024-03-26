3bc7a1c8a01327b3ff160fc2358a06044ccf4393
William Boen <133681097+willbo1103@users.noreply.github.com>
2024-03-26 14:04:57 -0400

    Update CPUView.py

--- src/views/CPUView.py
+++ src/views/CPUView.py
@@ -10,14 +10,11 @@ class CPUView:
         controller = c
         pass
 
-    '''
-    class CPUView:
-	def __init__(self):
-		self.PC = PC # PC counter. Starts at 0x0.
-		self.registerArr = [("{0:08b}".format(0))] * 32 # all 31 registers, r0-r31. This code inits blank values first.
-		self.memArr = memArr # Emulate memory, somehow. Check the PDF I suppose?
-		self.ALUCount = ALUCount # Count arithimetic operations (note: there are some beyond the obvious, ex. beq)
-		self.readWriteCount = readWriteCount # num of reads (load) and writes (store). Might need to count them separately?
-		self.instrCount = instrCount # write this value as an array instead with the size of the array as the
-		# number of instructions we support for this project. We want to store a count for each instruction's usage. 
-    '''
+    def __init__(self):
+	self.PC = PC # PC counter. Starts at 0x0.
+	self.registerArr = [("{0:08b}".format(0))] * 32 # all 31 registers, r0-r31. This code inits blank values first.
+	self.memArr = memArr # Emulate memory, somehow. Check the PDF I suppose?
+	self.ALUCount = ALUCount # Count arithimetic operations (note: there are some beyond the obvious, ex. beq)
+	self.readWriteCount = readWriteCount # num of reads (load) and writes (store). Might need to count them separately?
+	self.instrCount = instrCount # write this value as an array instead with the size of the array as the
+	# number of instructions we support for this project. We want to store a count for each instruction's usage. 
