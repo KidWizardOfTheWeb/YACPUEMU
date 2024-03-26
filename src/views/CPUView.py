2e0adc10f4dccbe96e223e6ffea76b7423e7d7ae
KidWizardOfTheWeb <smasih2020@gmail.com>
2024-03-26 08:19:08 -0400

    Updated view notes one more time

--- src/views/CPUView.py
+++ src/views/CPUView.py
@@ -14,10 +14,10 @@ class CPUView:
     class CPUView:
 	def __init__(self):
 		self.PC = PC # PC counter. Starts at 0x0.
-		self.registerArr = [31] # all 31 registers, r0-r31
-		self.memArr = memArr # Emulate memory, somehow.
+		self.registerArr = arr = [("{0:08b}".format(0))] * 32 # all 31 registers, r0-r31. This inits blank values first.
+		self.memArr = memArr # Emulate memory, somehow. Check the PDF I suppose?
 		self.ALUCount = ALUCount # Count arithimetic operations (note: there are some beyond the obvious, ex. beq)
-		self.readWriteCount = readWriteCount # num of reads (load byte/half/word) and writes
+		self.readWriteCount = readWriteCount # num of reads (load) and writes (store). Might need to count them separately?
 		self.instrCount = instrCount # write this value as an array instead with the size of the array as the
 		# number of instructions we support for this project. We want to store a count for each instruction's usage. 
     '''
