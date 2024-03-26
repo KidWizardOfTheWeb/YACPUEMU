b514ddffd485f46a15a2236a274ca748d9cdf00c
KidWizardOfTheWeb <smasih2020@gmail.com>
2024-03-26 16:38:04 -0400

    Updated model again

--- src/models/CPU.py
+++ src/models/CPU.py
@@ -9,26 +9,29 @@ class Instruction:
         # That is to say, in each case, init whatever is needed directly in there. Up to 3 total parameters usually.
         match self.opcode:
             case 0:
-                # SPECIAL. Only operates with registers, no immediates.
+                # R(egister)-Type. SPECIAL. Only operates with registers, no immediates.
                 # add, sub, and, or, slt
                 # The opcode starts with 0, but the final 6 bits are the actual instruction
-                reg1 = '{0:032b}'.format(binary)[6:11]
-                reg2 = '{0:032b}'.format(binary)[11:16]
-                storeReg = '{0:032b}'.format(binary)[16:21]
+                self.reg1 = '{0:032b}'.format(binary)[6:11]
+                self.reg2 = '{0:032b}'.format(binary)[11:16]
+                self.storeReg = '{0:032b}'.format(binary)[16:21]
                 # [21:26] is always padded with 5 zeroes (00000)
                 instruction = '{0:032b}'.format(binary)[26:32]
                 pass
             case 2:
-                # Jump
+                # J(ump)-type. Jump
                 pass
             case 3:
-                # Jump and link. We really should not end up here.
+                # J(ump)-type. Jump and link. We really should not end up here.
                 pass
             case _:
-                # Everything greater than 3 goes here.
+                # I(mmediate)-Type. Everything greater than 3 goes here.
                 # Lw, Sw, beq, addi
                 # First 6 bits are the actual instruction
                 instruction = self.opcode
+                self.reg1 = '{0:032b}'.format(binary)[6:11]
+                self.reg2 = '{0:032b}'.format(binary)[11:16]
+                self.offImmVal = '{0:032b}'.format(binary)[16:32] # 16-bit offset/immediate value
                 pass
         self.instruction = instruction
 
